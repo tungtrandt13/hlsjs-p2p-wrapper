744e1bd86e3bf9d39e3f35bbd2ca6b8ef5c2e733
Stephan Hesse <tchakabam@gmail.com>
2016-07-28 17:23:41 +0200

    XHRShaper 1.x needs call to useGlobal to shim XHR object

--- test/html/tests.js
+++ test/html/tests.js
@@ -3,7 +3,7 @@ window._TEST_ = true;
 window._ENVIRONMENT_ = 'development';
 
 require("babel-polyfill");
-require("xhr-shaper");
+require("xhr-shaper").useGlobal();
 
 require("./p2p-loader-generator");
 require("./bundle");
