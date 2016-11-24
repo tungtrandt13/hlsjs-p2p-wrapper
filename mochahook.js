e9545952e225c4985b2ad6112e918861a3bee8b8
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-11-24 15:25:01 +0300

    fixed test launching
    Was necessary because of `streamroot-p2p` import in tested files

--- mochahook.js
+++ mochahook.js
@@ -1,5 +1,19 @@
+import jsdom from 'jsdom';
+
 require('should');
 require("babel-core/register");
 
-global.window = {};
-window.XMLHttpRequest = {};
+global.jsdom = jsdom.jsdom;
+global.document = global.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
+
+/**
+ * Unsuported jsom globals
+ */
+
+global.Blob = () => {};
+global.Worker = () => {};
+global.URL = {
+    createObjectURL: () => 'fakeUrl',
+    revokeObjectURL: () => {},
+};
