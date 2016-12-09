97e2ed92d6defcb0132ea495eb99d54793c0e871
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-12-09 16:46:45 +0400

    finished test

--- test/html/wrapper.js
+++ test/html/wrapper.js
@@ -12,16 +12,21 @@ describe("HlsjsP2PWrapper", function() { // using plain ES5 function here
             p2pConfig:{
                 streamrootKey: "ry-v7xuywnt",
                 debug: true,
-            }
+            },
+            contentUrl: 'http://www.streambox.fr/playlists/test_001/stream.m3u8',
         };
     });
 
-    it("should return null if peerAgent is not initialized", (done) => {
+    it("should return null as peerAgent public API while peer agent is not initialized", (done) => {
         let hls = new Hls();
         let wrapper = new HlsjsP2PWrapper(config.p2pConfig, hls);
         should.equal(wrapper.peerAgent, null);
 
-        done();
+        hls.loadSource(config.contentUrl);
+        hls.on(Hls.Events.MANIFEST_PARSED, () => {
+            should.exist(wrapper.peerAgent);
+            done();
+        });
     });
 
 });
