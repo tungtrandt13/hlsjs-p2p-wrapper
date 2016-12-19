79a68c00a9c653e81ce6ae02feb74b383f20204f
Ruslan Gilmutdinov <ruslandinov@gmail.com>
2016-12-20 02:08:53 +0400

    building integration tests with Webpack

---
+++ webpack/webpack.test.babel.js
@@ -0,0 +1,37 @@
+import webpack from 'webpack';
+import { baseConfig, ROOT_PATH } from './webpack.base';
+import parseArgv from './tools/parseArgv';
+
+const { version } = require(`${ROOT_PATH}/package.json`);
+
+export default Object.assign(baseConfig, {
+    entry: [`${ROOT_PATH}/test/html/tests.js`],
+
+    output: {
+        library: [
+            'Tests'
+        ],
+        libraryTarget: 'umd',
+        path: `${ROOT_PATH}/test/html`,
+        filename: 'build.js',
+    },
+
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(true),
+        new webpack.optimize.UglifyJsPlugin({
+            sourceMap: false,
+            compress: {
+                drop_console: true,
+                warnings: false,
+            },
+            mangle: true,
+            output: {
+                comments: false,
+            },
+        }),
+        new webpack.DefinePlugin({
+            _VERSION_: JSON.stringify(version),
+        }),
+    ]
+});
